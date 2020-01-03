88cf63f6d0c2b7bbf9b303b3a8b8fdeb7366cff6
FocusLCDs <55808311+FocusLCDs@users.noreply.github.com>
2020-01-03 15:01:44 -0700

    Add files via upload

---
+++ ILI9163.cpp
@@ -0,0 +1,193 @@
+
+/*
+
+Adapted register settings for the ILI9163 driver IC and tested on the ST Nucleo-L476RG
+
+ **************************************************************************/
+
+#include "ILI9163.h"
+#include <limits.h>
+#include "pins_arduino.h"
+#include "wiring_private.h"
+#include "SPI.h"
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_GFX.h>
+#define SPI_DEFAULT_FREQ 80000000 ///< Default SPI data clock frequency
+
+
+
+ILI9163::ILI9163(uint16_t w, uint16_t h, int8_t cs, int8_t dc, int8_t mosi,
+  int8_t sclk, int8_t rst, int8_t miso) : Adafruit_SPITFT(ILI9163_TFTWIDTH_128,
+  ILI9163_TFTHEIGHT_160, cs, dc, mosi, sclk, rst, miso) {
+}
+
+
+ILI9163::ILI9163(uint16_t w, uint16_t h, int8_t cs, int8_t dc, int8_t rst) :
+  Adafruit_SPITFT(ILI9163_TFTWIDTH_128, ILI9163_TFTHEIGHT_160, cs, dc, rst) {
+}
+
+
+
+ILI9163::ILI9163(uint16_t w, uint16_t h, SPIClass *spiClass,
+  int8_t cs, int8_t dc, int8_t rst) : Adafruit_SPITFT(ILI9163_TFTWIDTH_128, ILI9163_TFTHEIGHT_160, spiClass, cs,
+  dc, rst) {
+}
+
+
+
+static const uint8_t PROGMEM Rcmd1[] = {                      
+    15,                             // 15 commands 
+    SWRESET,   ST_CMD_DELAY, 		//  1: Software reset
+      50,                           
+    SLPOUT,    ST_CMD_DELAY, 		//  2: Out of sleep mode
+      50,                           
+    FRMCTR1, 3,             //  3: Framerate ctrl - normal mode
+      0x08, 0x02, 0x02,             
+    FRMCTR2, 3,             //  4: Framerate ctrl - idle mode
+      0xB2, 0xB2, 0xB2,             
+    FRMCTR3, 6,             //  5: Framerate - partial mode
+      0x08, 0x02, 0x02,             
+      0x08, 0x02, 0x02,            
+    INVCTR,  1,             //  6: Display inversion ctrl
+      0xB4,                        
+    PWCTR1,  3,             //  7: Power control
+      0x0A,
+      0x02,                        
+      0x84,                         
+    PWCTR2,  1,             //  8: Power control
+      0x02,                         
+    PWCTR3,  2,             //  9: Power control
+      0x02,                         
+      0x00,                         
+    PWCTR4,  2,             // 10: Power control
+      0x8A,                         
+      0x2A,                         
+    PWCTR5,  2,             // 11: Power control
+      0x8A, 0xEE,
+    VMCTR1,  1,             // 12: Power control
+      0x50,
+    INVOFF,  0,              		// 13: Don't invert display, no args
+    MADCTL,  1,             		// 14: Mem access ctl directions
+      0x08,                         
+    COLMOD,  1,              		// 15: 16-bit color mode
+      0x05 },                       
+
+
+
+  Rcmd2[] = {                       
+    4,              	
+	
+    GMCTRP1, 15     ,       		//  1: Gamma Adjustments, pos. polarity 
+      0x36, 0x29, 0x12, 0x22,       
+      0x1C, 0x15, 0x42, 0xB7,       
+      0x2F, 0x13, 0x12, 0x0A,
+      0x11, 0x0B, 0x06, 
+    GMCTRN1, 15      ,      		//  2: Gamma Adjustments, neg. polarity
+      0x09, 0x16, 0x2D, 0x0D,       
+      0x13, 0x15, 0x40, 0x48,       
+      0x53, 0x0C, 0x1D, 0x25,
+      0x2E, 0x34, 0x39, 
+
+    NORON,     ST_CMD_DELAY, 		//  3: Normal display on
+      10,                           
+    DISPON,    ST_CMD_DELAY, 		//  4: Main screen turn on
+      100 };                        
+
+
+
+void ILI9163::displayInit(const uint8_t *addr) {
+
+  uint8_t  numCommands, cmd, numArgs;
+  uint16_t ms;
+
+  numCommands = pgm_read_byte(addr++);   // Number of commands to follow
+  while(numCommands--) {                 // For each command...
+    cmd = pgm_read_byte(addr++);         // Read command
+    numArgs  = pgm_read_byte(addr++);    // Number of args to follow
+    ms       = numArgs & ST_CMD_DELAY;   // If hibit set, delay follows args
+    numArgs &= ~ST_CMD_DELAY;            // Mask out delay bit
+    sendCommand(cmd, addr, numArgs);
+    addr += numArgs;
+
+    if(ms) {
+      ms = pgm_read_byte(addr++); // Read post-command delay time (ms)
+      if(ms == 255) ms = 255;     // If 255, delay for 255 ms
+      delay(ms);
+    }
+  }
+}
+
+
+void ILI9163::begin(uint32_t freq) {
+  if(!freq) {
+    freq = SPI_DEFAULT_FREQ;
+  }
+  _freq = freq;
+
+  invertOnCommand  = INVON;
+  invertOffCommand = INVOFF;
+
+  initSPI(freq, spiMode);
+}
+
+void ILI9163::commonInit(const uint8_t *cmdList) {
+  begin();
+
+  if(cmdList) {
+    displayInit(cmdList);
+  }
+}
+
+
+void ILI9163::setAddrWindow(uint16_t x, uint16_t y, uint16_t w,
+  uint16_t h) {
+  x += _xstart;
+  y += _ystart;
+  uint32_t xa = ((uint32_t)x << 16) | (x+w-1);
+  uint32_t ya = ((uint32_t)y << 16) | (y+h-1); 
+
+  writeCommand(CASET); // Column addr set
+  SPI_WRITE32(xa);
+
+  writeCommand(RASET); // Row addr set
+  SPI_WRITE32(ya);
+
+  writeCommand(RAMWR); // write to RAM
+}
+
+
+
+void ILI9163::setColRowStart(int8_t col, int8_t row) {
+  _colstart = col;
+  _rowstart = row;
+}
+
+void ILI9163::enableDisplay(boolean enable) {
+  sendCommand(enable ? DISPON : DISPOFF);
+}
+
+
+
+void ILI9163::enableTearing(boolean enable) {
+  sendCommand(enable ? TEON : TEOFF);
+}
+
+
+
+void ILI9163::initR() {
+	commonInit(Rcmd1);
+	displayInit(Rcmd2);
+
+
+	uint8_t madctl = 0;
+	madctl = MADCTL_MX | MADCTL_MY | MADCTL_BGR;
+    
+	_height = ILI9163_TFTHEIGHT_160;
+	_width  = ILI9163_TFTWIDTH_128;
+     
+    _xstart   = _colstart;
+    _ystart   = _rowstart;
+	 
+	 sendCommand(MADCTL, &madctl, 1);
+}
+
---
+++ ILI9163.h
@@ -0,0 +1,94 @@
+/*
+More register settings modified for the ILI9163 driver IC.
+Adapted from the ST77535 register settings
+
+Tested on ST Nucleo-L476RG microcontroller and created for 1.8" TFT 
+Display:
+https://focuslcds.com/product/1-8-tft-display-no-tp-e17rg11216lw6m300-n
+
+=========================================================================
+*/
+
+#include "Arduino.h"
+#include "Print.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+
+
+#define ILI9163_TFTWIDTH_128   128 
+#define ILI9163_TFTHEIGHT_160  160 
+
+#define ST_CMD_DELAY      0x80    
+#define SWRESET    0x01
+
+
+#define SLPOUT     0x11
+#define NORON      0x13
+#define INVOFF     0x20
+#define INVON      0x21
+#define DISPOFF    0x28
+#define DISPON     0x29
+#define CASET      0x2A
+#define RASET      0x2B
+#define RAMWR      0x2C
+
+
+#define TEOFF      0x34
+#define TEON       0x35
+#define MADCTL     0x36  
+#define COLMOD     0x3A 
+
+#define MADCTL_BGR 0x08 
+#define MADCTL_MY  0x80  
+#define MADCTL_MX  0x40	
+
+#define FRMCTR1    0xB1
+#define FRMCTR2    0xB2
+#define FRMCTR3    0xB3
+#define INVCTR     0xB4
+#define DISSET5    0xB6
+
+#define PWCTR1     0xC0
+#define PWCTR2     0xC1
+#define PWCTR3     0xC2
+#define PWCTR4     0xC3
+#define PWCTR5     0xC4
+#define VMCTR1     0xC5
+
+#define GMCTRP1    0xE0
+#define GMCTRN1    0xE1
+
+
+
+class ILI9163 : public Adafruit_SPITFT {
+	public: 
+	ILI9163(uint16_t w, uint16_t h, int8_t _CS, int8_t _DC,
+      int8_t _MOSI, int8_t _SCLK, int8_t _RST = -1, int8_t _MISO = 12);
+	  
+	   ILI9163(uint16_t w, uint16_t h, int8_t CS, int8_t RS,
+      int8_t RST = -1);
+	  
+    ILI9163(uint16_t w, uint16_t h, SPIClass *spiClass,
+      int8_t CS, int8_t RS, int8_t RST = -1);
+
+
+	void    initR();
+	
+	void setAddrWindow(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+
+    void enableDisplay(boolean enable);
+    void enableTearing(boolean enable);
+
+ protected:
+    uint8_t _colstart = 0, ///< Some displays need this changed to offset
+            _rowstart = 0, ///< Some displays need this changed to offset
+            spiMode   = SPI_MODE2; ///< Certain display needs MODE3 instead was 0, 2 works
+
+    void    begin(uint32_t freq = 0);
+    void    commonInit(const uint8_t *cmdList);
+    void    displayInit(const uint8_t *addr);
+    void    setColRowStart(int8_t col, int8_t row);
+
+};
